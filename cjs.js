8f38441722b812debbf50b8383c94cfcaf8b8b53
ben hockey <neonstalwart@gmail.com>
2011-06-22 14:49:50 -0400

    add code to prevent sourceURL trick in IE with conditional comments. thanks @jrburke

--- cjs.js
+++ cjs.js
@@ -7,20 +7,28 @@ define(function () {
 			// fetch the raw text
 			require(['text!' + target + '.js'], function (source) {
 				// start with the "standard" dependencies
-				var deps = ['require', 'exports', 'module'];
+				var deps = ['require', 'exports', 'module'],
+					wrapped;
 
 				// scan the source for other dependencies
 				source.replace(commentRegExp, '').replace(cjsRequireRegExp, function (match, dep) {
 					deps.push('cjs!' + dep);
 				});
 
-				// simulate injecting the wrapped source
-				load.fromText('cjs!' + target, "define(['" + deps.join("','") + "'],function (req, exports, module) {" + 
+				wrapped = "define(['" + deps.join("','") + "'],function (req, exports, module) {" + 
 					"function require(id) {" +
 						"return req('cjs!'+ id);" +
 					"}" + 
 					source + 
-				'});\n//@ sourceURL=' + require.toUrl(target));
+				'});';
+
+				// workaround IE conditional comments
+				/*@if (@_jscript) @else @*/
+				wrapped += '\n//@ sourceURL=' + require.toUrl(target);
+				/*@end@*/
+
+				// simulate injecting the wrapped source
+				load.fromText('cjs!' + target, wrapped);
 
 				// get the module we just defined and return it via load
 				require(['cjs!' + target], load);
